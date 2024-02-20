cebcb5677a1737a28d8e16fc919b25dd518202f3
adasjo <adasjo@live.se>
2024-02-20 02:04:41 +0100

    Add new tests for index_vars_to_types() function

--- tests/tensor/test_subtensor.py
+++ tests/tensor/test_subtensor.py
@@ -9,6 +9,7 @@ from numpy.testing import assert_array_equal
 import pytensor
 import pytensor.scalar as scal
 import pytensor.tensor.basic as ptb
+import pytensor.tensor as pt
 from pytensor import function
 from pytensor.compile import DeepCopyOp, shared
 from pytensor.compile.io import In
@@ -2683,19 +2684,62 @@ def test_pprint_IncSubtensor(indices, set_instead_of_inc, exp_res):
 def test_index_vars_to_types():
     x = ptb.as_tensor_variable(np.array([True, False]))
 
+    # Test line 1
+    # isinstance(entry, (np.ndarray, Variable))
+    # and hasattr(entry, "dtype")
+    # and entry.dtype == "bool"
     with pytest.raises(AdvancedIndexingError):
         index_vars_to_types(x)
-
+    
+    # Test line (New) 2
+    # isinstance(entry, Variable) and (
+    # entry.type in invalid_scal_types or entry.type in invalid_tensor_types
+    x = ptb.as_tensor_variable(1.4)
     with pytest.raises(TypeError):
-        index_vars_to_types(1)
-
-    res = index_vars_to_types(iscalar)
-    assert isinstance(res, scal.ScalarType)
-
+        index_vars_to_types(x)
+    
+    # Test line 590 3
+    # isinstance(entry, Variable) and entry.type in scal_types
     x = scal.constant(1, dtype=np.uint8)
     assert isinstance(x.type, scal.ScalarType)
     res = index_vars_to_types(x)
-    assert res == x.type
+    assert res == x.type 
+
+    # Test line 4
+
+
+    # Test line (New) 5
+    # isinstance(entry, Variable)
+    # and entry.type in tensor_types
+    # and all(entry.type.broadcastable) 
+    y = pt.tensor(dtype='int32', shape=(), name='t1')
+    res = index_vars_to_types(y)
+    z = pt.scalar('t2', dtype='int32')
+    z = pt.iscalar('t2')    
+    assert str(res) == str(z.type.dtype)
+
+    # Test line 598 6
+    # isinstance(entry, Type) and entry in tensor_types and all(entry.broadcastable)
+    res = index_vars_to_types(iscalar)
+    assert isinstance(res, scal.ScalarType)
+    
+    # Test lin 606 7, 9 & 11 (New)
+    # slice_ok and isinstance(entry, slice)
+    res = index_vars_to_types(slice(None, None))
+    assert isinstance(res, slice)
+    assert res == slice(None, None)
+
+    # Test line 13 (New)
+    # slice_ok and isinstance(entry, slice) but where C is None
+    res = index_vars_to_types(slice(None, None, None))
+    assert isinstance(res, slice)
+    assert res == slice(None, None, None)
+    
+
+    # Test line 630 14
+    with pytest.raises(TypeError):
+        index_vars_to_types(1)
+
 
 
 @pytest.mark.parametrize(
