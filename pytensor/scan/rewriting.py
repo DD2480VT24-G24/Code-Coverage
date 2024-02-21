3212b3946af5fc07167976c153ecaa3f883d4da5
Victor Österman <vios@kth.se>
2024-02-21 16:04:50 +0100

    fix: renamed the function calls in belongs_to_set to acctually call a function

--- pytensor/scan/rewriting.py
+++ pytensor/scan/rewriting.py
@@ -1998,10 +1998,10 @@ class ScanMerge(GraphRewriter):
         op = node.op
         rep_node = set_nodes[0]
         rep_op = rep_node.op
-        if (self.firstCheck(op, rep_op)):
+        if (self.first_check(op, rep_op)):
             return False
 
-        nsteps, rep_nsteps = self.parseSteps(node, rep_node)
+        nsteps, rep_nsteps = self.parse_steps(node, rep_node)
 
         if nsteps != rep_nsteps:
             return False #exit point
